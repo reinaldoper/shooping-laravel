4eef38880a55521baeac5558e4ea949723ac79f2
reinaldoper <reinaldoper83@gmail.com>
2023-10-19 13:46:00 -0400

    refactor: ajusta url

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://shooping-laravel.vercel.app'),
 
     'asset_url' => env('ASSET_URL', '/public/'),
 
