dee018e0ecda119a8df1c4cb464a7e0ed5c95cbe
reinaldoper <reinaldoper83@gmail.com>
2023-10-19 17:36:36 -0400

    refactor: mais um ajuste no css

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,11 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ config('app.name')}}::@yield('titulo')</title>
-  <link href="{{ asset('/css/style.css') }}" rel="stylesheet" type="text/css" >
+  <link href={{ asset('/css/style.css') }} rel="stylesheet" type="text/css" >
 </head>
 <body>
   <div class="logo">
-    <img src="{{ asset('/img/update.png')}}" alt="Digital">
+    <img src={{ asset('/img/update.png')}} alt="Digital">
   </div>
   <div class="container">
     @yield('conteudo')
