1d9e4318491f5ed6648eba8331853429983c81c9
reinaldoper <reinaldoper83@gmail.com>
2023-10-20 12:08:04 -0400

    feat: volta css file

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ config('app.name')}}::@yield('titulo')</title>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
 </head>
 <body>
   <div class="logo">
